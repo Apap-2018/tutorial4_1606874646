364f5517b0d60440a31c1c6edc6daa7853406f01
Dimas Praharsa <cervmkiii@gmail.com>
2018-10-04 17:40:11 +0700

    first commit tutorial 4

---
+++ src/main/java/com/apap/tutorial4/controller/CarController.java
@@ -0,0 +1,44 @@
+package com.apap.tutorial4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import com.apap.tutorial4.model.*;
+import com.apap.tutorial4.service.*;
+
+@Controller
+public class CarController {
+	
+	@Autowired
+	private CarService carService;
+	
+	@Autowired
+	private DealerService dealerService;
+	
+
+	@RequestMapping(value = "/car/add{dealerId}", method = RequestMethod.GET)
+	private String add(@PathVariable(value = "dealerId")Long dealerId, Model model) {
+		CarModel car = new CarModel();
+		DealerModel dealer = dealerService.getDealerDetailById(dealerId).get();
+		car.setDealer(dealer);
+		
+		model.addAttribute("car", car);
+		return "addCar";
+	}
+	
+	@RequestMapping(value = "/car/add", method = RequestMethod.POST)
+	private String addCarSubmit(@ModelAttribute CarModel car) {
+		carService.addCar(car);
+		return "add";
+	}
+
+	@RequestMapping(value = "/car/delete/{id}", method = RequestMethod.GET)
+	private String deleteCar(@PathVariable long id) {
+		CarModel car = carService.getCar(id);
+		carService.deleteCar(car);
+		return "delete";
+}
+}
---
+++ src/main/java/com/apap/tutorial4/controller/DealerController.java
@@ -0,0 +1,54 @@
+package com.apap.tutorial4.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apap.tutorial4.model.DealerModel;
+import com.apap.tutorial4.service.CarService;
+import com.apap.tutorial4.service.DealerService;
+
+@Controller
+public class DealerController {
+	@Autowired
+	private DealerService dealerService;
+	
+	@Autowired
+	private CarService carService;
+	
+	@RequestMapping("/")
+	private String home() {
+		return "home";
+	}
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.GET)
+	private String add(Model model) {
+		model.addAttribute("dealer", new DealerModel());
+		return "addDealer";
+	}
+	
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.POST)
+	private String addDealerSubmit(@ModelAttribute DealerModel dealer) {
+		dealerService.addDealer(dealer);
+		return "add";
+	}
+	@RequestMapping("/dealer/view")
+	public String viewid(@RequestParam(value="dealerId",required = true) Long id, Model model) {
+		DealerModel archive = dealerService.getDealerDetailById(id).get();
+//		DealerModel archive = dealerService.getDealerDetailById(id).get();
+		
+//		if (id.is){
+//			model.addAttribute("error",true);
+//		return "eror";
+//		}
+		
+		model.addAttribute("dealer",archive);
+		return "view-dealer";
+	}
+}
